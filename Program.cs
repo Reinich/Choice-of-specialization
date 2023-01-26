1ed3d47a11566676aaddf70fe19b58a020a47907
Reni <reinich@outlook.com>
2023-01-26 17:53:31 +0300

    Add Input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray(int n)
+{
+    string[] list = new string[n];
+    list = test.Split(new Char[] {' '});
+    return (list);    
+}
+
+void FindThreeElements(string[] list)
+{
+    foreach(string item in list)
+    {
+        int num = int.Parse(item);
+
+    }
+}
+
+
+
+
+Console.Clear();
+Console.Write("Введите количество значений: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(InputArray(n));
+
+
+
+
+
